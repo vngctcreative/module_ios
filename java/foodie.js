a00952690b2f808560bfcec8021b62bbdf4c5fcb
Creative <115936592+vngctcreative@users.noreply.github.com>
2025-10-20 00:36:53 +0700

    Create foodie.js

---
+++ java/foodie.js
@@ -0,0 +1,17 @@
+var objc = JSON.parse($response.body);
+
+    objc = {
+  "result" : {
+    "activated" : true,
+    "products" : [
+      {
+        "managed" : false,
+        "status" : "ACTIVE",
+        "startDate" : 1666666666666,
+        "productId" : "com.linecorp.Foodiecn.subscribe.oneyear",
+        "expireDate" : 4092599349000
+      }
+    ]
+  }
+}
+$done({body : JSON.stringify(objc)});
